3a416d3633ed129f6de6afcdd1c21fac8009a6ba
Midou-bit <elhas@DESKTOP-2AHVSLS.localdomain>
2025-12-17 15:58:35 +0100

    activite3

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\IndexController;
 
-Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+
+
+//Route::get('/', function () {
+    //return Inertia::render('welcome', [
+       // 'canRegister' => Features::enabled(Features::registration()),
+    //]);
+//})->name('home');
+
+Route::get('/', [IndexController::class, 'index']);
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
