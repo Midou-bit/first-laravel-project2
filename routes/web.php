e5ffb28f637e3b2d2c98e65030834af2d487c2d1
Midou-bit <elhas@DESKTOP-2AHVSLS.localdomain>
2025-12-17 17:08:58 +0100

    correction complete

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
-use App\Http\Controllers\IndexController;
-
-
-
-//Route::get('/', function () {
-    //return Inertia::render('welcome', [
-       // 'canRegister' => Features::enabled(Features::registration()),
-    //]);
-//})->name('home');
 
 Route::get('/', [IndexController::class, 'index']);
 
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
-
-require __DIR__.'/settings.php';
