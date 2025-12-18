2bd75e408be0f785ed19c7442780fe7849bdbe29
Midou-bit <elhas@DESKTOP-2AHVSLS.localdomain>
2025-12-18 15:44:28 +0100

    404 test

---
+++ resources/views/errors/not-found.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <title>Page non trouvée</title>
+</head>
+<body>
+
+    <h1>Page non trouvée</h1>
+
+</body>
+</html>
