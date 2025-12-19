8b4f662b9dfd5d1a2788847500a075d71f9acff2
Midou-bit <elhas@DESKTOP-2AHVSLS.localdomain>
2025-12-19 10:10:01 +0100

    Activité 12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,20 +5,12 @@
 @section('content')
     <h2>Bienvenue sur la page d’accueil</h2>
     
-     <p>Bienvenue sur le site de {{ $name }}</p>
+    <p>Bienvenue sur le site de {{ $name }}</p>
 
-    <x-article 
-        title="L’IA soigne mieux"
-        description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite."
-    />
-
-    <x-article 
-        title="Villes vertes"
-        description="Les métropoles deviennent plus écologiques et durables."
-    />
-
-    <x-article 
-        title="Télétravail"
-        description="Plus de liberté, mais aussi plus de solitude."
-    />
+    @foreach ($articles as $article)
+        <x-article 
+            :title="$article['title']"
+            :description="$article['description']"
+        />
+    @endforeach
 @endsection
